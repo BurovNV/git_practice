12d44b73eadad69484d9792e230e7818dbe61450
host <host@mail.ru>
2024-01-10 21:50:00 +0300

    fix main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def say_hello():
     print('hello!')
 
 
-if __name__ ==  '__main__':
+if __name__ == '__main__':
+    for i in range(3):
+        say_hello()
     for i in range(3):
         say_hello()
