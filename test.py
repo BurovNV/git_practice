d5f2371201f504ed0ad3f55ea0c29514496bcfd7
host <host@mail.ru>
2024-01-11 13:22:38 +0300

    test is ok

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-if __name__ == '__main__':
-    print("it's OK!")
+def gen():
+    import random
+    while True:
+        yield random.randint(0, 100)
 
+
+if __name__ == '__main__':
+    g = gen()
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    print("it's OK!")
