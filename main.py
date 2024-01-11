6228ac016ff70074e90944858854dbfbe96a4b88
Burov Nikita <51811820+BurovNV@users.noreply.github.com>
2024-01-11 19:31:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def say_hello():
     print('hello!')
 
 
+def what_time():
+    import time
+    print(time.time())
+
+
 if __name__ == '__main__':
     for i in range(3):
         say_hello()
+    what_time()
